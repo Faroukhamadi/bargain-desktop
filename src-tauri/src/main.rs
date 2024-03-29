f2a1e64c30f0d791cc29204666b4b42249375d41
faroukhamadi <faroukhamadi1@outlook.com>
2024-03-29 22:22:01 +0100

    add initial read file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::fs;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![read_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn read_file(path: String) -> Result<String, String> {
+    match fs::read_to_string(path) {
+        Ok(content) => Ok(content),
+        Err(e) => Err(e.to_string()),
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	const readFile = async () => {
+		try {
+			let res = await invoke('read_file', { path: '/tmp/bargain/messages_123.json' });
+			res = JSON.parse(res);
+			return res;
+		} catch (error) {
+			console.error('error: ', error);
+		}
+	};
+</script>
+
+<button on:click={readFile}>Read file</button>
