c0a88ff848f8162ea1b449692a87ecd67e69f6b7
faroukhamadi <faroukhamadi1@outlook.com>
2024-04-20 21:31:01 +0100

    remove prompt deleting

--- src/routes/chat/[id]/+page.svelte
+++ src/routes/chat/[id]/+page.svelte
@@ -304,27 +304,6 @@
 								{@html renderMarkdown(question.data.content)}
 							</div>
 						</div>
-						{#if i === history.length - 1 && !isLoading}
-							<div style="width: 100%; text-align: right;">
-								<button
-									disabled={isLoading}
-									class="btn-ghost btn-sm btn"
-									on:click|preventDefault={() => deletePrompt('1', i)}
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 16 16"
-										width="16"
-										height="16"
-									>
-										<path
-											class="fill-base-content"
-											d="M11 1.75V3h2.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75ZM4.496 6.675l.66 6.6a.25.25 0 0 0 .249.225h5.19a.25.25 0 0 0 .249-.225l.66-6.6a.75.75 0 0 1 1.492.149l-.66 6.6A1.748 1.748 0 0 1 10.595 15h-5.19a1.75 1.75 0 0 1-1.741-1.575l-.66-6.6a.75.75 0 1 1 1.492-.15ZM6.5 1.75V3h3V1.75a.25.25 0 0 0-.25-.25h-2.5a.25.25 0 0 0-.25.25Z"
-										/>
-									</svg>
-								</button>
-							</div>
-						{/if}
 					</div>
 				{:else if question.type === 'ai'}
 					<div class="w-10/12 mx-auto sm:w-10/12 chat chat-start py-4">
@@ -348,28 +327,6 @@
 								{@html renderMarkdown(question.data.content)}
 							</div>
 						</div>
-						{#if i === history.length - 1 && !isLoading}
-							<div style="width: 100%; text-align: right;">
-								<button
-									disabled={isLoading}
-									class="btn-ghost btn-sm btn"
-									on:click|preventDefault={() => deletePrompt('1', i)}
-								>
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 16 16"
-										width="16"
-										height="16"
-									>
-										<path
-											class="fill-base-content"
-											d="M11 1.75V3h2.25a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1 0-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75ZM4.496 6.675l.66 6.6a.25.25 0 0 0 .249.225h5.19a.25.25 0 0 0 .249-.225l.66-6.6a.75.75 0 0 1 1.492.149l-.66 6.6A1.748 1.748 0 0 1 10.595 15h-5.19a1.75 1.75 0 0 1-1.741-1.575l-.66-6.6a.75.75 0 1 1 1.492-.15ZM6.5 1.75V3h3V1.75a.25.25 0 0 0-.25-.25h-2.5a.25.25 0 0 0-.25.25Z"
-										/>
-									</svg>
-									<span class="sr-only">Delete</span>
-								</button>
-							</div>
-						{/if}
 					</div>
 				{:else if question.type === 'system'}
 					<div class="text-md w-full px-10 pt-2 pb-4 text-center font-light md:px-16">
